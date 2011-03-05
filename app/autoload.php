9b0aa2441f8307887074d0753b6f9f09ae5e5df3
dbu <david.buchmann@liip.ch>
2011-03-05 10:57:17 +0100

    adding FunctionalTestBundle to submodules

--- app/autoload.php
+++ app/autoload.php
@@ -6,6 +6,7 @@ $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony'                        => __DIR__.'/../vendor/symfony/src',
     'Bundle'                         => __DIR__.'/../src',
+    'Liip'                           => __DIR__.'/../src',
     'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine/common/lib',
     'Doctrine\\ODM\\PHPCR'           => __DIR__.'/../vendor/doctrine-phpcr/lib',
     'Zend'                           => __DIR__.'/../vendor/zend/library',
