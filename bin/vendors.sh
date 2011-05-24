91ad52161209b873009c5c755cfaf9b7e71ad722
Daniel Barsotti <daniel.barsotti@liip.ch>
2011-05-24 09:41:08 +0200

    Added PhpcrCommandsBundle to vendor script and to AppKernel

--- bin/vendors.sh
+++ bin/vendors.sh
@@ -84,6 +84,7 @@ mkdir -p Symfony/Cmf/Bundle
 cd Symfony/Cmf/Bundle
 install_git CoreBundle git://github.com/symfony-cmf/CoreBundle.git
 install_git NavigationBundle git://github.com/symfony-cmf/NavigationBundle.git
+install_git PhpcrCommandsBundle git://github.com/sixty-nine/PhpcrCommandsBundle.git
 cd ../../..
 
 
