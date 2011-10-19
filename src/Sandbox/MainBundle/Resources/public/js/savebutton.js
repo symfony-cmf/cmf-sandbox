1a3028e1ee2a4660a71229f2c9f671e33d9272aa
Colin Frei <colin.frei@liip.ch>
2011-10-19 15:10:31 +0200

    A bit more style for the top save bar, and autosave

--- src/Sandbox/MainBundle/Resources/public/js/savebutton.js
+++ src/Sandbox/MainBundle/Resources/public/js/savebutton.js
@@ -1,25 +1,77 @@
 jQuery(document).ready(function($) {
-    // catch the event saying the editable was opened
-    // This doesn't catch NEW fields
     var saveTimestamp = new Date().getTime();
     var contentModified = false;
     var timeUpdateInterval = null;
+    var saveTimeoutSet = false;
 
-    var that = this;
-
-    function updateTimeText(text) {
-        $('#saveStatus').text(text);
+    function updateTimeText(html) {
+        $('#saveStatus').html(html);
     }
 
-    function contentModified() {
-        console.log('content modified');
+    function convertTimestampToHTML5(timestamp) {
+        function addLeadingZero(nr) {
+            if (nr.toString().length == 1) {
+                nr = "0" + nr;
+            }
+
+            return nr;
+        }
+
+        var dateObject = new Date(timestamp);
+
+        var returnValue = dateObject.getFullYear();
+        returnValue += "-";
+        returnValue += addLeadingZero(dateObject.getMonth() + 1);
+        returnValue += "-";
+        returnValue += addLeadingZero(dateObject.getDate());
+        returnValue += "T";
+        returnValue += addLeadingZero(dateObject.getHours());
+        returnValue += ":";
+        returnValue += addLeadingZero(dateObject.getMinutes());
+        returnValue += ":";
+        returnValue += addLeadingZero(dateObject.getSeconds());
+
+        var timezoneOffset = dateObject.getTimezoneOffset()
+        if (timezoneOffset === 0) {
+            returnValue += "Z";
+        } else {
+            if (timezoneOffset >= 1) {
+                returnValue += "+";
+            } else {
+                returnValue += "-";
+                timezoneOffset = timezoneOffset * -1;
+            }
+
+            returnValue += addLeadingZero(timezoneOffset / 60);
+            returnValue += ":";
+            returnValue += addLeadingZero(timezoneOffset % 60);
+        }
+        console.log(returnValue);
+        return returnValue;
+    }
+
+    function contentChanged() {
         contentModified = true;
 
+        if (saveTimestamp == 0) {
+            saveTimestamp = new Date().getTime();
+        }
+
         var secondsAgo = Math.round((new Date().getTime() - saveTimestamp) / 1000);
-        updateTimeText("Last Save was <span time=\"" + saveTimestamp + "\">" + secondsAgo + "</span> seconds ago"); // Text should not be in JS!
+
+        var dateObject = new Date(saveTimestamp);
+        updateTimeText("Last save was <time datetime=\"" + convertTimestampToHTML5(saveTimestamp) + "\">" + secondsAgo + "</span> seconds ago <button class=\"saveButton\">Save Now</button>"); // Text should not be in JS!
+
+        if (!saveTimeoutSet) {
+            setTimeout(function() {
+                vieSaveContent();
+                saveTimeoutSet = false;
+            }, 5000);
+            saveTimeoutSet = true;
+        }
     }
 
-    function vieSavedSuccess() {
+    function vieSaveSuccess() {
         updateTimeText("Saved!"); // Text should not be in JS!
 
         setTimeout(function() {
@@ -28,26 +80,34 @@ jQuery(document).ready(function($) {
             }
         }, 5000);
 
-        saveTimestamp = new Date().getTime();
+        saveTimestamp = 0;
     }
 
-    function vieSavedStart() {
+    function vieSaveStart() {
         timeUpdateInterval = null;
         updateTimeText("Saving..."); // Text should not be in JS!
         contentModified = false;
     }
 
     function removeHandlers() {
-        $(this).unbind("hallodeactivated", that.removeHandlers);
-        $(this).unbind("hallomodified", that.contentModified);
+        $(this).unbind("hallodeactivated", removeHandlers);
+        $(this).unbind("hallomodified", contentModified);
     }
 
     function addHandlers() {
-        $(this).bind("hallodeactivated", that.removeHandlers);
-        $(this).bind("hallomodified", that.contentModified);
-        $(document).bind("vieSavedStart", that.vieSavedStart);
-        $(document).bind("vieSavedSuccess", that.vieSavedSuccess);
+        $(this).bind("hallodeactivated", removeHandlers);
+        $(this).bind("hallomodified", contentChanged);
+        $(document).bind("vieSaveStart", vieSaveStart);
+        $(document).bind("vieSaveSuccess", vieSaveSuccess);
+
     }
 
+    // This doesn't catch NEW fields
     $('[contenteditable]').bind("halloactivated", addHandlers);
+    $('.saveButton').live('click', vieSaveContent);
+    $('#saveStatus').click(function() {
+        if ($(this).find('.saveButton').length < 1) {
+            $('#saveStatus').append(' <button class=\"saveButton\">Save Now</button>');
+        }
+    });
 });
