cdb8ae0ef624345f915e3b986096449d2ada13cb
Colin Frei <colin.frei@liip.ch>
2011-10-20 15:47:40 +0200

    Change to just a button whos' text changes, and add some (bad) styling to it

--- src/Sandbox/MainBundle/Resources/public/js/savebutton.js
+++ src/Sandbox/MainBundle/Resources/public/js/savebutton.js
@@ -1,66 +1,31 @@
 jQuery(document).ready(function($) {
-    var saveTimestamp = new Date().getTime();
-    var contentModified = false;
-    var timeUpdateInterval = null;
     var saveTimeoutSet = false;
 
-    function updateTimeText(html) {
-        $('#saveStatus').html(html);
+    function updateButton(action) {
+        switch (action) {
+            case 'savenow':
+                $('.saveButton').html("Save now");
+                $('.saveButton').addClass('saveButtonActive');
+            break;
+
+            case 'saved':
+                $('.saveButton').html("Saved");
+                $('.saveButton').removeClass('saveButtonActive');
+            break;
+
+            case 'saving':
+                $('.saveButton').html("Saving...");
+                $('.saveButton').removeClass('saveButtonActive');
+            break;
+        }
     }
 
-    function convertTimestampToHTML5(timestamp) {
-        function addLeadingZero(nr) {
-            if (nr.toString().length == 1) {
-                nr = "0" + nr;
-            }
-
-            return nr;
-        }
-
-        var dateObject = new Date(timestamp);
-
-        var returnValue = dateObject.getFullYear();
-        returnValue += "-";
-        returnValue += addLeadingZero(dateObject.getMonth() + 1);
-        returnValue += "-";
-        returnValue += addLeadingZero(dateObject.getDate());
-        returnValue += "T";
-        returnValue += addLeadingZero(dateObject.getHours());
-        returnValue += ":";
-        returnValue += addLeadingZero(dateObject.getMinutes());
-        returnValue += ":";
-        returnValue += addLeadingZero(dateObject.getSeconds());
-
-        var timezoneOffset = dateObject.getTimezoneOffset()
-        if (timezoneOffset === 0) {
-            returnValue += "Z";
-        } else {
-            if (timezoneOffset >= 1) {
-                returnValue += "+";
-            } else {
-                returnValue += "-";
-                timezoneOffset = timezoneOffset * -1;
-            }
-
-            returnValue += addLeadingZero(timezoneOffset / 60);
-            returnValue += ":";
-            returnValue += addLeadingZero(timezoneOffset % 60);
-        }
-        console.log(returnValue);
-        return returnValue;
+    function doUpdateButton(event) {
+        updateButton(event.data.action);
     }
 
     function contentChanged() {
-        contentModified = true;
-
-        if (saveTimestamp == 0) {
-            saveTimestamp = new Date().getTime();
-        }
-
-        var secondsAgo = Math.round((new Date().getTime() - saveTimestamp) / 1000);
-
-        var dateObject = new Date(saveTimestamp);
-        updateTimeText("Last saved <time datetime=\"" + convertTimestampToHTML5(saveTimestamp) + "\">a few seconds ago</time> <button class=\"saveButton\">Save Now</button>"); // Text should not be in JS!
+        updateButton('savenow');
 
         if (!saveTimeoutSet) {
             setTimeout(function() {
@@ -71,43 +36,20 @@ jQuery(document).ready(function($) {
         }
     }
 
-    function vieSaveSuccess() {
-        updateTimeText("Saved!"); // Text should not be in JS!
-
-        setTimeout(function() {
-            if (!contentModified) {
-                updateTimeText("All changes saved");
-            }
-        }, 5000);
-
-        saveTimestamp = 0;
-    }
-
-    function vieSaveStart() {
-        timeUpdateInterval = null;
-        updateTimeText("Saving..."); // Text should not be in JS!
-        contentModified = false;
-    }
-
     function removeHandlers() {
         $(this).unbind("hallodeactivated", removeHandlers);
-        $(this).unbind("hallomodified", contentModified);
+        $(this).unbind("hallomodified", contentChanged);
     }
 
     function addHandlers() {
         $(this).bind("hallodeactivated", removeHandlers);
         $(this).bind("hallomodified", contentChanged);
-        $(document).bind("vieSaveStart", vieSaveStart);
-        $(document).bind("vieSaveSuccess", vieSaveSuccess);
+        $(document).bind("vieSaveStart", {"action": "saving"}, doUpdateButton);
+        $(document).bind("vieSaveSuccess", {"action": "saved"}, doUpdateButton);
 
     }
 
     // This doesn't catch NEW fields
     $('[contenteditable]').bind("halloactivated", addHandlers);
     $('.saveButton').live('click', vieSaveContent);
-    $('#saveStatus').click(function() {
-        if ($(this).find('.saveButton').length < 1) {
-            $('#saveStatus').append(' <button class=\"saveButton\">Save Now</button>');
-        }
-    });
 });
