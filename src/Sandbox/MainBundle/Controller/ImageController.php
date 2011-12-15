d0c14183cf08afb04fdc03ea30d71252d4339ba3
Fabian Vogler <fabian@equivalence.ch>
2011-12-15 10:14:39 +0100

    CMFE-69 Display image

--- src/Sandbox/MainBundle/Controller/ImageController.php
+++ src/Sandbox/MainBundle/Controller/ImageController.php
@@ -9,6 +9,7 @@ use Doctrine\ODM\PHPCR\DocumentManager;
 use FOS\RestBundle\View\ViewHandlerInterface;
 use FOS\RestBundle\View\View;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -40,23 +41,46 @@ class ImageController extends Controller
         $this->viewHandler = $viewHandler;
     }
 
+    public function displayAction($id)
+    {
+        $basepath = $this->container->getParameter('symfony_cmf_content.static_basepath');
+
+        $image = $this->dm->find(null, $basepath.'/'.$id);
+
+        $data = stream_get_contents($image->content);
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'image/jpeg');
+        $response->setPublic();
+
+        $date = new \DateTime();
+        $date->setTimestamp(time() + 332640000);
+        $response->setExpires($date);
+        $response->setMaxAge(332640000);
+
+        return $response;
+    }
+
     public function uploadAction(Request $request)
     {
         $basepath = $this->container->getParameter('symfony_cmf_content.static_basepath');
-        $error = '';
+        $error = false;
         $path = '';
 
         $files = $request->files;
 
+        $id = '';
         foreach ($files->all() as $file ) {
             if (in_array($file->getClientMimeType(), $this->images_mime)) {
                 $name = $file->getClientOriginalName();
                 $path = $file->getPathname();
-                $jcrPath = $basepath.'/'.md5($name);
+                $id = md5(time());
+                $jcrPath = $basepath.'/'.$id;
                 if (!$this->dm->find(null, $jcrPath)) {
                     $image = new Image();
                     $image->setPath($jcrPath);
-                    $image->setContent(file_get_contents($path));
+                    $image->content = file_get_contents($path);
+                    $image->tags = explode(',', $request->get('tags'));
                 } else {
                     $error = 'This file already exists in your backend.';
                 }
@@ -69,9 +93,6 @@ class ImageController extends Controller
 
         $this->dm->flush();
 
-        $data = array("path" => $path, "error" => $error);
-
-        $view = View::create($data);
-        return $this->viewHandler->handle($view);
+        return $this->redirect($this->generateUrl('image_display', array('id' => $id)));
     }
 }
