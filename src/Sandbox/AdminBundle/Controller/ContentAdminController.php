2c114d63ae7a21085ba76923e8ba972da2012e3f
Nacho Martín <nitram.ohcan@gmail.com>
2011-12-30 05:13:59 +0100

    Populate identifier when creating a new node in the admin bundle tree (see #36)

--- src/Sandbox/AdminBundle/Controller/ContentAdminController.php
+++ src/Sandbox/AdminBundle/Controller/ContentAdminController.php
@@ -21,4 +21,62 @@ class ContentAdminController extends Controller
         $params['treeroot'] = $this->content_path;
         return parent::render($template, $params, $response);
     }
+
+    /**
+     * TODO: this is a temp hack in the Sandbox. This should go into the 
+     * base class in the SAB CRUD controller we are extending here.
+     *
+     * return the Response object associated to the create action
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createAction()
+    {
+        if (false === $this->admin->isGranted('CREATE')) {
+            throw new AccessDeniedException();
+        }
+
+        $id = $this->get('request')->get($this->admin->getIdParameter());
+
+        $object = $this->admin->getNewInstance();
+        $idField = $this->admin->getModelManager()->getModelIdentifier($this->admin->getClass());
+        $object->{'set'.$idField}($id);
+
+        $this->admin->setSubject($object);
+
+        $form = $this->admin->getForm();
+        $form->setData($object);
+
+        if ($this->get('request')->getMethod() == 'POST') {
+            $form->bindRequest($this->get('request'));
+
+            if ($form->isValid()) {
+                $this->admin->create($object);
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array(
+                        'result' => 'ok',
+                        'objectId' => $this->admin->getNormalizedIdentifier($object)
+                    ));
+                }
+
+                $this->get('session')->setFlash('sonata_flash_success','flash_create_success');
+                // redirect to edit mode
+                return $this->redirectTo($object);
+            }
+            $this->get('session')->setFlash('sonata_flash_error', 'flash_create_error');
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getEditTemplate(), array(
+            'action' => 'create',
+            'form'   => $view,
+            'object' => $object,
+        ));
+    }
+
 }
