701e99a046cd3cf9967bb90aecf6ba9a13d7d912
Stefano Sala <stefano.sala@spa.it>
2012-05-17 22:58:24 +0200

    Cleaning database and loading fixtures

--- app/tests/HomepageTest.php
+++ app/tests/HomepageTest.php
@@ -1,19 +1,56 @@
 <?php
 
 use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Doctrine\Common\DataFixtures\Executor\PHPCRExecutor;
+use Doctrine\Common\DataFixtures\Purger\PHPCRPurger;
 
 class HomepageTest extends WebTestCase
 {
-	public function testRedirectToDefaultLanguage()
-	{
-		$client = $this->createClient();
+    public static function setUpBeforeClass()
+    {
+        static::purgeAndloadFixtures(array(
+            'Sandbox\MainBundle\DataFixtures\PHPCR\LoadStaticPageData',
+            'Sandbox\MainBundle\DataFixtures\PHPCR\LoadRoutingData',
+            'Sandbox\MainBundle\DataFixtures\PHPCR\LoadMenuData',
+        ));
+    }
 
-		$client->request('GET', '/');
+    public function testRedirectToDefaultLanguage()
+    {
+        $client = $this->createClient();
 
-		$this->assertEquals(301, $client->getResponse()->getStatusCode());
+        $client->request('GET', '/');
 
-		$client->followRedirect();
+        $this->assertEquals(301, $client->getResponse()->getStatusCode());
 
-		$this->assertEquals('http://localhost/en', $client->getRequest()->getUri());
-	}
+        $client->followRedirect();
+
+        $this->assertEquals('http://localhost/en', $client->getRequest()->getUri());
+    }
+
+    protected static function purgeAndloadFixtures(array $fixtures)
+    {
+        static::createClient();
+
+        $fixturesToLoad = array();
+
+        foreach ($fixtures as $fixture) {
+            if (!class_exists($fixture)) {
+                throw new \Exception(sprintf('Class %s not exists.', $fixture));
+            }
+
+            $fixtureToLoad = new $fixture;
+            $fixtureToLoad->setContainer(static::$kernel->getContainer());
+
+            $fixturesToLoad[] = $fixtureToLoad;
+        }
+
+        $registry = static::$kernel->getContainer()->get('doctrine_phpcr');
+        $dm = $registry->getManager(null);
+
+        $purger = new PHPCRPurger($dm);
+
+        $executor = new PHPCRExecutor($dm, $purger);
+        $executor->execute($fixturesToLoad);
+    }
 }
