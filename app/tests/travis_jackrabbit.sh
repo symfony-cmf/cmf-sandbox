aafca2f0ff185f2190a433e166eafc6826f77ba8
Henri Bergius <henri.bergius@iki.fi>
2012-06-04 18:40:43 +0200

    Don't install Jackrabbit version with --dev so we don't need Midgard2

--- app/tests/travis_jackrabbit.sh
+++ app/tests/travis_jackrabbit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp app/config/parameters.yml.dist app/config/parameters.yml
-php composer.phar install --dev
+php composer.phar install
 ./vendor/jackalope/jackalope-jackrabbit/bin/jackrabbit.sh
