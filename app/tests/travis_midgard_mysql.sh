5e72032cd2eb678e8617aaa08612b763c03276df
Henri Bergius <henri.bergius@iki.fi>
2012-06-04 18:59:12 +0200

    Ensure we have the correct workspace in DB

--- app/tests/travis_midgard_mysql.sh
+++ app/tests/travis_midgard_mysql.sh
@@ -16,3 +16,4 @@ php composer.phar update --dev
 
 # Ensure the database is properly created
 app/console doctrine:phpcr:register-system-node-types
+php app/console doctrine:phpcr:workspace:create sandbox_test
