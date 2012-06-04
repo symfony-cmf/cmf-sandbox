383508ee8f61e99d243b302c7ff9a3680033d58c
Henri Bergius <henri.bergius@iki.fi>
2012-06-04 19:11:19 +0200

    Move workspace creation to individual transport scripts

--- app/tests/travis_jackrabbit.sh
+++ app/tests/travis_jackrabbit.sh
@@ -2,3 +2,5 @@
 cp app/config/parameters.yml.dist app/config/parameters.yml
 php composer.phar update
 ./vendor/jackalope/jackalope-jackrabbit/bin/jackrabbit.sh
+
+php app/console doctrine:phpcr:workspace:create sandbox_test -e=test
