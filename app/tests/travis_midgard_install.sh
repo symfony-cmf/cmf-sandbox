c2f9ff337321935c86132074c4b338f867190277
lsmith77 <smith@pooteeweet.org>
2012-06-07 00:39:29 +0200

    first install midgard .. then run composer update

--- app/tests/travis_midgard_install.sh
+++ app/tests/travis_midgard_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-php composer.phar require midgard/phpcr:dev-master
-php composer.phar update
-
 # Download installation script from phpcr-midgard2
 wget https://raw.github.com/midgardproject/phpcr-midgard2/master/tests/travis_midgard.sh
 chmod +x ./travis_midgard.sh
@@ -11,3 +8,6 @@ chmod +x ./travis_midgard.sh
 # Copy PHPCR schemas to Midgard's global schema dir
 sudo wget --directory-prefix=/usr/share/midgard2/schema https://github.com/midgardproject/phpcr-midgard2/raw/master/data/share/schema/midgard_namespace_registry.xml
 sudo wget --directory-prefix=/usr/share/midgard2/schema https://github.com/midgardproject/phpcr-midgard2/raw/master/data/share/schema/midgard_tree_node.xml
+
+php composer.phar require midgard/phpcr:dev-master
+php composer.phar update
