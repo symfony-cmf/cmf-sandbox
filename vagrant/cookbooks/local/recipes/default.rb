6c6e26658a6ef6b8184eb1b66805f704ce822209
waldvogel <waldvogel@liip.ch>
2012-07-31 19:38:23 +0200

    vagrant: user/group not set to copy config files

--- vagrant/cookbooks/local/recipes/default.rb
+++ vagrant/cookbooks/local/recipes/default.rb
@@ -40,8 +40,6 @@ end
 { "/vagrant/app/config/parameters.yml.dist" =>  "/vagrant/app/config/parameters.yml",
   "/vagrant/app/config/phpcr_jackrabbit.yml.dist" => "/vagrant/app/config/phpcr.yml" }.each do | src, dest |
   file dest  do
-    owner "vagrant"
-    group "vagrant"
     content IO.read(src)
   end
 end
@@ -53,7 +51,7 @@ execute "date.timezone = UTC in php.ini?" do
 end
 
 bash "Running composer install in separate vendor directory" do
-  not_if "test -d /var/tmp/vendor"
+  not_if "test -f /var/tmp/vendor/autoload.php"
   user "vagrant"
   cwd "/var/tmp"
   code <<-EOH
@@ -64,12 +62,6 @@ bash "Running composer install in separate vendor directory" do
     php composer.phar install
   EOH
 end
-
-# execute "Bind mount vendor folder" do
-#   not_if "mount|grep vendor|grep -v grep"
-#   user "root"
-#   command "mount --bind /var/tmp/vendor /vagrant/vendor"
-# end
 
 bash "Preparing the phpcr repository" do
   not_if "test -L /vagrant/web/bundles/framework"
@@ -87,43 +79,3 @@ bash "Preparing the phpcr repository" do
     app/console -v doctrine:phpcr:fixtures:load
   EOH
 end
-
-
-# begin
-#   private_key = node['ssh_key']['private']
-#   public_key = node['ssh_key']['public']
-
-#   dir = "/home/vagrant/.ssh"
-
-#   file "#{dir}/id_rsa" do
-#     owner "vagrant"
-#     group "vagrant"
-#     mode "600"
-#     content private_key
-#   end
-
-#   file "#{dir}/id_rsa.pub" do
-#     owner "vagrant"
-#     group "vagrant"
-#     mode "644"
-#     key = node['ssh_key']['public']
-#     content public_key
-#   end
-# rescue
-# end
-
-# if FileTest.exists?("/vagrant/carlo.sql.gz")
-#   bash "copy database" do
-#     # dont if the db already exists
-#     not_if("/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf -e'show databases' | grep carlo", :user => 'root')
-#     user "root"
-#     group "root"
-#     code <<-EOH
-#     mysql --defaults-file=/etc/mysql/debian.cnf -e 'create database carlo' && \
-#     zcat /vagrant/carlo.sql.gz | mysql --defaults-file=/etc/mysql/debian.cnf --one-database carlo && \
-#     mysql --defaults-file=/etc/mysql/debian.cnf -e "update carlo.core_config_data set value = 'http://carloag.lo:8080/shop/' where path like 'web/%secure/base_url'"
-#   EOH
-#   end
-# else
-#   log "No database dump found (carlo.sql.gz)"
-# end
