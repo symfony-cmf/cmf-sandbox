e8b93256905f2bf1828d276e7ca2ff859a050ab1
Uwe Jäger <uwej711@googlemail.com>
2012-09-10 14:06:16 +0200

    use phpcr admin base
    to get rid of leading slash issue

--- src/Sandbox/MainBundle/Admin/ContentAdmin.php
+++ src/Sandbox/MainBundle/Admin/ContentAdmin.php
@@ -2,7 +2,7 @@
 
 namespace Sandbox\MainBundle\Admin;
 
-use Sonata\AdminBundle\Admin\Admin;
+use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
