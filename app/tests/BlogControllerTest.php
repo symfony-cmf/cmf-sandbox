7266ade22bd54a851b32982ea5d5aebf8ee02559
dantleech <dan.t.leech@gmail.com>
2013-02-05 15:22:31 +0000

    Added frontend tests

--- app/tests/BlogControllerTest.php
+++ app/tests/BlogControllerTest.php
@@ -2,17 +2,17 @@
 
 namespace Sandbox;
 
-class BlogAdminTest extends WebTestCase
+class BlogControllerTest extends WebTestCase
 {
     public function testList()
     {
         $client = $this->createClient();
 
-        $client->request('GET', '/en/admin/bundle/blog/blog/list');
+        $crawler = $client->request('GET', '/en/blog');
 
         $response = $client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Blogs', $response->getContent());
+        $this->assertGreaterThan(1, $crawler->filter('div.post')->count());
     }
 }
 
