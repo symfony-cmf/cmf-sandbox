1f8b814879c398f4fbe2c38c8994ec78ce26fb4d
dantleech <daniel@ylyad.fr>
2013-02-08 10:23:28 +0000

    Fixed tests

--- app/tests/BlogControllerTest.php
+++ app/tests/BlogControllerTest.php
@@ -11,6 +11,7 @@ class BlogControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/en/blog');
 
         $response = $client->getResponse();
+
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertGreaterThan(1, $crawler->filter('div.post')->count());
     }
