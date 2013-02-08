06b25bacffb18f77e98a10f41217a7e62df14bb1
dantleech <daniel@ylyad.fr>
2013-02-08 09:20:05 +0000

    Fixed up fixtures

--- src/Sandbox/MainBundle/DataFixtures/PHPCR/LoadBlogData.php
+++ src/Sandbox/MainBundle/DataFixtures/PHPCR/LoadBlogData.php
@@ -56,57 +56,31 @@ class LoadBlogData extends ContainerAware implements FixtureInterface, OrderedFi
     protected function getTags()
     {
         $tags = array(
-            'DropBox',
-            'XMPP',
-            'android',
             'apache',
-            'archos',
-            'audacious',
-            'awesome',
-            'bash',
-            'bootstrap',
-            'bristol',
-            'diagramming',
-            'doctrine',
-            'doctrine2',
-            'git',
-            'gloucester',
-            'graphs',
-            'gt540',
-            'jack',
-            'javascript',
-            'mail',
-            'manchester',
-            'mapdroyd',
-            'markdown',
-            'mongodb',
-            'paris',
             'php',
-            'profiling',
-            'projectm',
-            'running',
-            'scripting',
-            'sed',
-            'software',
-            'design',
-            'ssh',
-            'sup',
-            'symfony',
+            'jackrabbit',
+            'phpcr',
+            'doctrine',
             'symfony2',
-            'thonon',
-            'touring',
-            'trainer',
-            'travel',
-            'twig',
-            'ubnutu',
-            'velo',
-            'vim',
-            'weymouth',
-            'workflow',
-            'xdebug',
-            'xml',
-            'ylly',
-            'yprox',
+            'routing',
+            'cms',
+            'cmf',
+            'content',
+            'midgard',
+            'doctrine-dbal',
+            'createjs',
+            'phpcr-odm',
+            'tree-browser',
+            'sonata-project',
+            'menu',
+            'search',
+            'jcr',
+            'jackalope',
+            'documentation',
+            'mongodb',
+            'couchdb',
+            'admin',
+            'blocks',
         );
 
         $nbTags = rand(2,5);
@@ -120,11 +94,20 @@ class LoadBlogData extends ContainerAware implements FixtureInterface, OrderedFi
 
     protected function getWords($nbWords = null)
     {
-        $projectNim = <<<HERE
-Project Nim was an attempt to go further than Project Washoe. Terrace and his colleagues aimed to use more rigorous experimental techniques, and the intellectual discipline of the experimental analysis of behavior, so that the linguistic abilities of the apes could be put on a more secure footing. Roger Fouts wrote: Since 98.7% of the DNA in humans and chimps is identical, some scientists (but not Noam Chomsky) believed that a chimp raised in a human family, and using ASL (American Sign Language), would shed light on the way language is acquired and used by humans. Project Nim, headed by behavioral psychologist Herbert Terrace at Columbia University, was conceived in the early 1970s as a challenge to Chomsky's thesis that only humans have language.[4] Attention was particularly focused on Nim's ability to make different responses to different sequences of signs and to emit different sequences in order to communicate different meanings. However, the results, according to Fouts, were not as impressive as had been reported from the Washoe project. Terrace, however, was skeptical of Project Washoe and, according to the critics, went to great lengths to discredit it. While Nim did learn 125 signs, Terrace concluded that he had not acquired anything the researchers were prepared to designate worthy of the name "language" (as defined by Noam Chomsky) although he had learned to repeat his trainers' signs in appropriate contexts. Language is defined as a "doubly articulated" system, in which signs are formed for objects and states and then combined syntactically, in ways that determine how their meanings will be understood. For example, "man bites dog" and "dog bites man" use the same set of words but because of their ordering will be understood by speakers of English as denoting very different meanings. For one thing, they say, there's no syntax — a basic requirement of language. Without combining words and then being able to switch combinations to change meaning, goes the argument, what animals use is more like a code than a language.[5] One of Terrace's colleagues, Laura-Ann Petitto, estimated that with more standard criteria, Nim's true vocabulary count was closer to 25 than 125. However, other students who cared for Nim longer than Petitto disagreed with her and with the way that Terrace conducted his experiment. Critics assert that Terrace used his analysis to destroy the movement of ape-language research. Terrace argued that none of the chimps were using language, because they could learn signs but could not form them syntactically as language, as described above. Terrace and his colleagues concluded that the chimpanzee did not show any meaningful sequential behavior that rivaled human grammar. Nim's use of language was strictly pragmatic, as a means of obtaining an outcome, unlike a human child's, which can serve to generate or express meanings, thoughts or ideas. There was nothing Nim could be taught that could not equally well be taught to a pigeon using the principles of operant conditioning. The researchers therefore questioned claims made on behalf of Washoe, and argued that the apparently impressive results may have amounted to nothing more than a "Clever Hans" effect, not to mention a relatively informal experimental approach
+        $text = <<<HERE
+This tutorial shows how to install the Symfony CMF Sandbox, a demo platform aimed at showing the tool's basic features running on a demo environment. This can be used to evaluate the platform or to see actual code in action, helping you understand the tool's internals.
+While it can be used as such, this sandbox does not intend to be a development platform. If you are looking for installation instructions for a development setup, please refer to:
+
+As Symfony CMF Sandbox is based on Symfony2, you should make sure you meet the Requirements for running Symfony2. Git 1.6+, Curl and PHP Intl are also needed to follow the installation steps listed below.
+If you wish to use Jackalope + Apache JackRabbit as the storage medium recommended, you will also need Java JRE. For other mechanisms and its requirements, please refer to their respective sections.
+
+ontent Repository API for Java JCR is a specification for a Java platform application programming interface API to access content repositories in a uniform manner.1dead link2not in citation given The content repositories are used in content management systems to keep the content data and also the metadata used in content management systems CMS such as versioning metadata. The specification was developed under the Java Community Process as JSR-170 Version 1.34 and as JSR-283 version 25 The main Java package is javax.jcr.
+
+he PHP Content Repository is an adaption of the Java Content Repository JCR standard, an open API specification defined in JSR-283. 
+The API defines how to handle hierarchical semi-structured data in a consistent way. The typical use case is content management systems. PHPCR combines the best out of document-oriented databases weak structured data and of XML databases hierarchical trees. On top of that, it adds useful features like searching, versioning, access control and locking on top of it.
 HERE;
 
-        $words = explode(' ', $projectNim);
+        $words = explode(' ', $text);
 
         $nbWords = $nbWords ? $nbWords : rand(100,500);
         $sWords = array();
