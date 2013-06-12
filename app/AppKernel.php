8b3a4cb63b16c3ec0393c9103a5a98c3e058cb0e
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-06-12 21:48:27 +0200

    updated dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,9 +17,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
             // enable cmf bundles
             new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
