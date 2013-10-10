e8bf7243da0575def3b304a702b318aa7bdadd1b
dantleech <dan.t.leech@gmail.com>
2013-10-10 21:37:30 +0200

    Added automated 200 testing for admin
    - Only works on non-parameterized routes.
    - Code is slightly ugly as we cannot determine if a route needs a POST.
    - The export route has an undefined requirement on "_format".

--- app/tests/AdminTest.php
+++ app/tests/AdminTest.php
@@ -2,50 +2,71 @@
 
 namespace Sandbox;
 
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
+
 class AdminTest extends WebTestCase
 {
-    public function testList()
+    protected $pool;
+    protected $router;
+
+    public function setUp()
     {
-        $client = $this->createClientAuthenticated();
-
-        $client->request('GET', '/en/admin/cmf/menu/menunode/list');
-
-        $response = $client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Menus', $response->getContent());
-
-        $menuCount = $this->isSearchSupported() ? 17 : 16;
-        $this->assertContains("$menuCount results", $response->getContent());
-        $this->assertContains('Explicit template', $response->getContent());
+        parent::setUp();
+        $this->pool = $this->getContainer()->get('sonata.admin.pool');
+        $this->router = $this->getContainer()->get('router');
+        $this->client = $this->createClientAuthenticated();
     }
 
-    public function testCreate()
+    public function testAdmin()
     {
-        $client = $this->createClientAuthenticated();
+        $adminGroups = $this->pool->getAdminGroups();
+        $admins = array();
 
-        $crawler = $client->request('GET', '/en/admin/cmf/menu/menunode/create');
+        foreach (array_keys($adminGroups) as $adminName) {
+            $admins = array_merge($admins, $this->pool->getAdminsByGroup($adminName));
+        }
 
-        $response = $client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Create', $response->getContent());
-
-        $this->assertCount(11, $crawler->filter('.control-group'));
-        $this->assertContains('Parent', $crawler->filter('.control-group')->first()->children()->first()->text());
-        $this->assertContains(' *', $crawler->filter('.control-group')->first()->children()->first()->text());
+        foreach ($admins as $admin) {
+            $this->doTestReachableAdminRoutes($admin);
+        }
     }
 
-    public function testEdit()
+    protected function doTestReachableAdminRoutes($admin)
     {
-        $client = $this->createClientAuthenticated();
+        $routeCollection = $admin->getRoutes();
 
-        $crawler = $client->request('GET', '/en/admin/cmf/menu/menunode/cms/menu/main/demo-item/external-item/edit');
+        foreach ($routeCollection->getElements() as $route) {
+            try {
+                $url = $this->router->generate($route->getDefault('_sonata_name'), array(
+                    '_locale' => 'en'
+                ));
+            } catch (MissingMandatoryParametersException $e) {
+                // do not try and load pages with parameters, e.g. edit, show, etc.
+                continue;
+            }
 
-        $response = $client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Edit &quot;external-item&quot;', $response->getContent());
+            $crawler = $this->client->request('GET', $url);
+            $res = $this->client->getResponse();
+            $statusCode = $res->getStatusCode();
 
-        $this->assertCount(11, $crawler->filter('.control-group'));
-        $this->assertContains('Parent', $crawler->filter('.control-group')->first()->children()->first()->text());
-        $this->assertContains(' *', $crawler->filter('.control-group')->first()->children()->first()->text());
+            // hack around apparently mal-defined routes
+            if ($statusCode != 200) {
+                $exceptionMessage = $crawler->filter('.text-exception h1')->html();
+
+                // we cannot determine if sonata routes need a POST
+                if (false !== strpos($exceptionMessage, 'POST expected')) {
+                    continue;
+                }
+
+                // the export route has no _format requirement
+                if (false !== strpos($exceptionMessage, 'Export in format `` is not allowed')) {
+                    continue;
+                }
+            }
+
+            $this->assertEquals(200, $statusCode, sprintf(
+                'URL %s returns 200 OK HTTP Code', $url
+            ));
+        }
     }
 }
