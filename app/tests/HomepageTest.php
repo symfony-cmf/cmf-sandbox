53b79f30d95443e494d603e1dc331358af7918e2
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-10-13 17:49:00 +0200

    [WIP] enabled json/xml support, this currently fails because the serializer does not yet support PHPCR ODM

--- app/tests/HomepageTest.php
+++ app/tests/HomepageTest.php
@@ -32,4 +32,23 @@ class HomepageTest extends WebTestCase
         $menuCount = $this->isSearchSupported() ? 17 : 16;
         $this->assertCount($menuCount, $crawler->filter('ul.menu_main li'));
     }
+
+    public function testJsonContents()
+    {
+        $client = $this->createClient();
+
+        $client->request(
+            'GET',
+            '/en',
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE'          => 'application/json',
+            )
+        );
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $json = @json_decode($client->getResponse()->getContent());
+        $this->assertTrue($json);
+    }
 }
