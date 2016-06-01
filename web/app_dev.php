c1901a0f040393a0ccfc8301f534a1dc8883c6fe
WouterJ <waldio.webdesign@gmail.com>
2016-06-01 14:37:56 +0200

    Sync with SE 3.1

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,27 +9,29 @@
  * file that was distributed with this source code.
  */
 
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
+// for more information
 //umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
-       '127.0.0.1',
-       '::1',
-       '172.22.22.1',
-    ))
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-
-use Symfony\Component\HttpFoundation\Request;
+/**
+ * @var Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
