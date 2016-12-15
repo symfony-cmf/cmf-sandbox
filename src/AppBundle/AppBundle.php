68fb547adb5701f6991a0e529f612aae63bea554
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-12-16 00:00:13 +0100

    replace class configuration by a compiler pass

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -11,8 +11,14 @@
 
 namespace AppBundle;
 
+use AppBundle\DependencyInjection\Compiler\DocumentClassPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DocumentClassPass());
+    }
 }
