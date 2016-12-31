1bd7e40cbe1dbeaa86e12f829d24a823b14df54a
ElectricMaxxx <maximilian.berghoff@gmx.de>
2017-01-01 00:49:45 +0100

    increase licence year

--- app/AppCache.php
+++ app/AppCache.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Symfony CMF package.
  *
- * (c) 2011-2015 Symfony CMF
+ * (c) 2011-2017 Symfony CMF
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
