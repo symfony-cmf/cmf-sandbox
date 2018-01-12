0604edaf4fbe1f45ec7637e511384d7e5ab28eb6
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-01-12 11:19:04 +0100

    fix the correct cache directory

--- bin/reloadFixtures.sh
+++ bin/reloadFixtures.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DIR=$1
+PROD=$2||false
 run () {
     comment="+++ "$1" +++"
     command=$2
@@ -21,10 +22,10 @@ run () {
 }
 
 CACHE_DIR=${DIR}var/cache/prod
-if [ -d "${CACHE_DIR}" ]; then
-    run  "Remove cache directory:" "rm -rf ${CACHE_DIR}"
-else
+if [ "${PROD}" ]; then
     run  "Remove cache directory:" "rm -rf /var/cache/prod"
+else
+    run  "Remove cache directory:" "rm -rf ${DIR}var/cache/prod"
 fi
 run "Drop and init dbal:" "php ${DIR}bin/console --env=prod doctrine:phpcr:init:dbal --drop --force -n -vvv"
 run "Init repositories:" "php ${DIR}bin/console --env=prod doctrine:phpcr:repository:init -n -vvv"
