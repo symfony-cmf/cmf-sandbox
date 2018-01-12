7b75b95d34f6bf1d5ca3036b45fab5a0b5ab0c87
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-01-12 08:48:48 +0100

    fix directory issue for removing c correct cache folder

--- bin/reloadFixtures.sh
+++ bin/reloadFixtures.sh
@@ -19,8 +19,8 @@ run () {
     fi
 }
 
-run  "Remove cache directory:" "rm -rf ${DIR}/var/cache/prod"
-run "Drop and init dbal:" "${DIR}/bin/console --env=prod doctrine:phpcr:init:dbal --drop --force -n"
-run "Init repositories:" "${DIR}/bin/console --env=prod doctrine:phpcr:repository:init -n"
-run "Load date fixtures:" "${DIR}/bin/console --env=prod doctrine:phpcr:fixtures:load -n"
-run "Warm up cache:" "${DIR}/bin/console --env=prod cache:warmup -n --no-debug"
+run  "Remove cache directory:" "rm -rf ${DIR}var/cache/prod"
+run "Drop and init dbal:" "${DIR}bin/console --env=prod doctrine:phpcr:init:dbal --drop --force -n"
+run "Init repositories:" "${DIR}bin/console --env=prod doctrine:phpcr:repository:init -n"
+run "Load date fixtures:" "${DIR}bin/console --env=prod doctrine:phpcr:fixtures:load -n"
+run "Warm up cache:" "${DIR}bin/console --env=prod cache:warmup -n --no-debug"
